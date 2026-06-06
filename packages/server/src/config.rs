583ffb4e586ff0fa2472abf23d59c8399296546d
Martin Page <wookoouk@gmail.com>
2026-06-06 14:01:59 +0100

    move plugins and registry to helpcore-plugins repo
    Plugin source code, the community registry, and the plugin CI workflow
    have been extracted to doomedramen/helpcore-plugins. The default registry
    URL has been updated to point to the new location.

--- packages/server/src/config.rs
+++ packages/server/src/config.rs
@@ -5,7 +5,7 @@ use std::io::Write;
 use std::path::{Path, PathBuf};
 
 pub const DEFAULT_REGISTRY_URL: &str =
-    "https://raw.githubusercontent.com/doomedramen/helpcore/main/registry/plugins.json";
+    "https://raw.githubusercontent.com/doomedramen/helpcore-plugins/main/registry/plugins.json";
 
 #[derive(Debug, Clone, Deserialize, Serialize)]
 pub struct Config {
